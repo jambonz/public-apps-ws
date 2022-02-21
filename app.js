ba4627d1d537cd4efa3ebc4350a825a30b0ee6c7
Dave Horton <daveh@beachdognet.com>
2022-02-21 15:33:11 -0500

    refactored a lot of functionality into @jambonz/node-client

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
+const {WebSocketServer} = require('ws');
+const {WsRouter, WsSession, handleProtocols} = require('@jambonz/node-client');
+const router = new WsRouter();
 const opts = Object.assign({
   timestamp: () => `, "time": "${new Date().toISOString()}"`,
   level: process.env.LOGLEVEL || 'info'
 });
 const logger = require('pino')(opts);
-const {WebSocketServer} = require('ws');
-const WsSession = require('./lib/ws-session');
 const port = process.env.WS_PORT || 8080;
-const Router = require('./lib/router');
-
-const handleProtocols = (protocols) => {
-  if (!protocols.has('ws.jambonz.org')) return false;
-  return 'ws.jambonz.org';
-};
-
 const wss = new WebSocketServer({ port, handleProtocols });
 
-const router = new Router();
-router.set(require('./lib/routes'));
+router.use(require('./lib/routes'));
 
-wss
-  .on('listening', () => {
-    logger.info(`websocket server listening on port ${port}`);
-  })
-  .on('connection', (ws, req) => {
-    new WsSession(logger, router, ws, req);
-  });
+wss.on('listening', () => {
+  logger.info(`websocket server listening on port ${port}`);
+});
+wss.on('connection', (ws, req) => {
+  new WsSession({logger, router, ws, req});
+});
